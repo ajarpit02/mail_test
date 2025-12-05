5c66cd85c3e7e4e10bf60a5ef48d557eb61c1047
ajarpit02 <arpit.jain@techolution.com>
2025-12-05 15:49:37 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,13 @@
+from utils import add
+
+def multiply(a, b):
+    return a * b
+
+def main():
+    x = 5
+    y = 3
+    print("Addition:", add(x, y))
+    print("Multiplication:", multiply(x, y))
+
+if __name__ == "__main__":
+    main()
